b9bebd975f270a5fb012cadca1dc86146e1bf01e
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-03-02 00:06:33 +0900

    fix script: break if error found

--- build_with_ferrite_darwin.sh
+++ build_with_ferrite_darwin.sh
@@ -2,7 +2,7 @@
 
 # For Building on Darwin Platform(macOS)
 
-cargo build --example with_ferrite --features with_ferrite,ferrite/VK_MVK_macos_surface
-install_name_tool -change @rpath/vulkan.framework/Versions/A/vulkan @executable_path/vulkan.framework/Versions/A/vulkan target/debug/examples/with_ferrite
-cp -r $VK_SDK_PATH/macOS/Frameworks/vulkan.framework target/debug/examples/
+cargo build --example with_ferrite --features with_ferrite,ferrite/VK_MVK_macos_surface &&
+install_name_tool -change @rpath/vulkan.framework/Versions/A/vulkan @executable_path/vulkan.framework/Versions/A/vulkan target/debug/examples/with_ferrite &&
+cp -r $VK_SDK_PATH/macOS/Frameworks/vulkan.framework target/debug/examples/ &&
 target/debug/examples/with_ferrite
