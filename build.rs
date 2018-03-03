6b767dfc2b1b69dc7d90abe304dd53a5badb30d5
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-03-04 00:05:44 +0900

    fix: platform-dependent configuration script

--- build.rs
+++ build.rs
@@ -1,16 +1,18 @@
+
+#[cfg(target_os = "macos")]
 fn main()
 {
-    if cfg!(target_os="macos")
-    {
-        println!("cargo:rustc-link-search=framework={}/MoltenVK/macOS", env!("VK_SDK_PATH"));
-        println!("cargo:rustc-link-search=framework={}/macOS/Frameworks", env!("VK_SDK_PATH"));
-        println!("cargo:rustc-link-lib=c++");
-        println!("cargo:rustc-link-lib=framework=IOSurface");
-        println!("cargo:rustc-link-lib=framework=IOKit");
-        println!("cargo:rustc-link-lib=framework=vulkan");
-    }
-    else if cfg!(windows)
-    {
-        println!("cargo:rustc-link-search=static={}/Lib", env!("VK_SDK_PATH"));
-    }
+    println!("cargo:rustc-link-search=framework={}/MoltenVK/macOS", env!("VK_SDK_PATH"));
+    println!("cargo:rustc-link-search=framework={}/macOS/Frameworks", env!("VK_SDK_PATH"));
+    println!("cargo:rustc-link-lib=c++");
+    println!("cargo:rustc-link-lib=framework=IOSurface");
+    println!("cargo:rustc-link-lib=framework=IOKit");
+    println!("cargo:rustc-link-lib=framework=vulkan");
 }
+#[cfg(windows)]
+fn main()
+{
+    println!("cargo:rustc-link-search=static={}/Lib", env!("VK_SDK_PATH"));
+}
+#[cfg(not(all(target_os = "macos", windows)))]
+fn main() {}
