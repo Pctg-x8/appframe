39f5666efda11cbc77982a944cbc81ea2f3e7d96
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-05-26 20:56:48 +0900

    fix: basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,13 +4,13 @@ use appframe::*;
 use std::rc::Rc;
 use std::cell::RefCell;
 
-pub struct Application(RefCell<Option<NativeWindow<Application>>>);
+pub struct Application(RefCell<Option<NativeWindow<EmptyWindowEventDelegate<Application>>>>);
 impl EventDelegate for Application
 {
     fn postinit(&self, srv: &Rc<GUIApplication<Self>>)
     {
         let w = NativeWindowBuilder::new(640, 360, "AppFrame basic example")
-            .resizable(false).transparent(true).create(&srv).expect("Creating MainWindow");
+            .resizable(false).transparent(true).create(&srv, &Rc::new(EmptyWindowEventDelegate::default())).expect("Creating MainWindow");
         *self.0.borrow_mut() = Some(w);
         self.0.borrow().as_ref().unwrap().show();
     }
